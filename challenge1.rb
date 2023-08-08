3bc277dae30db0aad4745f05e7ec14d21bd831b3
Billy Burch <billyburch40@gmail.com>
2023-08-08 17:23:45 -0600

    Initial commit

---
+++ challenge1.rb
@@ -0,0 +1,3 @@
+# For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**. 
+
+# 1. Given an array of strings, print only the strings that have exactly 4 characters.
