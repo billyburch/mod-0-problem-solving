ceea30f65c3997d8135d4e7488c431997e178b00
Billy Burch <billyburch40@gmail.com>
2023-08-08 17:34:56 -0600

    add comments and hobbies array

---
+++ challenge2.rb
@@ -0,0 +1,16 @@
+# For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**. 
+
+# 1. Start with an array of hobbies. Print out only the words that end in "ing".
+
+# Overall Goal: Decide on an array of strings (hobbies) and assign that to a variable. We will then need to apply iteration and a built-in method to print from the array, only those strings ending in suffix "ing."
+
+# Pseudocode: 
+# - Assign an array of strings to a variable using the assignment operator and square brackets.
+# - Write an .each method, name each element in the array
+# - Use an if statement with the include and suffix methods to print only those array elements ending in "ing" 
+
+#Final solution
+
+hobbies = ["guitar playing", "skiing", "meditation", "yardwork", "movies"]
+
+p hobbies
