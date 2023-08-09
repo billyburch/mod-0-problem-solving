72184e3884e7fd81ddcc95c85dd389c7c9744bb0
Billy Burch <billyburch40@gmail.com>
2023-08-08 19:08:39 -0600

    add overall goal and pseudocode for challenge 3

---
+++ challenge3.rb
@@ -0,0 +1,13 @@
+# For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**. 
+
+# 1. Start with an array of travel destinations. Print every travel destination in alphabetical order embedded in a sentence using string interpolation. For example, if the destination is "New York City", print something like "The next place I want to visit is New York City!" 
+
+# Overall Goal: Decide on an array of strings (travel destinations) and assign that to a variable. We will need to sort the elements alphaetically by first letter, apply iteration and write a method, including index positions and interpolation to print from the array, a sentence that includes each travel destination.
+
+# Pseudocode: 
+# - Assign an array of strings to a variable using the assignment operator and square brackets.
+# - use the sort method to alphabetize elements
+# - write an each_index method that will interpolate each string into the same sentence when that method is called
+
+#Final solution
+
